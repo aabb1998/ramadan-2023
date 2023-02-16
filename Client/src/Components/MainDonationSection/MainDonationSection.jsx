f0257e3d05dafabb0cbf806253a66e962aaa1ce3
Abdul Abbou <53510613+aabb1998@users.noreply.github.com>
2023-02-16 21:12:16 +1100

    Add readmore.

--- Client/src/Components/MainDonationSection/MainDonationSection.jsx
+++ Client/src/Components/MainDonationSection/MainDonationSection.jsx
@@ -5,8 +5,20 @@ import "./Styles.css";
 
 const MainDonationSection = () => {
 	const [loaded, setLoaded] = useState(false);
+	const [expanded, setExpanded] = useState(false);
+
 	const handleImageLoad = () => {
 		setLoaded(true);
+	};
+
+	const text =
+		"Lorem ipsum dolor sit amet, consectetur adipiscing elit. Praesent quis neque ac magna scelerisque blandit. Nullam vitae velit nibh. Donec vel quam a libero tempor semper vel vel ex. Vestibulum ante ipsum primis in faucibus orci luctus et ultrices posuere cubilia curae; Mauris ultricies augue eu enim sagittis, in dictum eros maximus. Duis non bibendum nibh, quis malesuada turpis. Phasellus eu ultrices dolor. Duis dignissim lectus eu massa lacinia ornare.";
+
+	const firstWords = text.split(" ").slice(0, 10).join(" ");
+	const remainingWords = text.split(" ").slice(10).join(" ");
+
+	const handleReadMoreClick = () => {
+		setExpanded(true);
 	};
 	return (
 		<>
@@ -15,10 +27,11 @@ const MainDonationSection = () => {
 					<div className="MainDonationSection-container">
 						<div className="MainDonationSection-header">
 							<h5>DONATE NOW!</h5>
-							<span>
+
+							{/* <span>
 								This holiday season, your gift will be matched
 								to provide double the lifesaving assistance.
-							</span>
+							</span> */}
 						</div>
 						<div className="MainDonationSection-img">
 							<img
@@ -29,10 +42,25 @@ const MainDonationSection = () => {
 						</div>
 						<div className="MainDonationSection-img-desc">
 							<h1>People that needs healthy food.</h1>
-							<span>
+							{/* <span>
 								Do I have consent to do what I lorem ipsum root
 								and branch review.
-							</span>
+							</span> */}
+							<div className="read-more-container">
+								<span>{expanded ? text : firstWords}</span>
+								{!expanded && (
+									<button onClick={handleReadMoreClick}>
+										Read More
+									</button>
+								)}
+								<span
+									className={`read-more ${
+										expanded ? "visible" : ""
+									}`}
+								>
+									{remainingWords}
+								</span>
+							</div>
 						</div>
 						<div>
 							<MainProgressBar />
