524faa8119be6f61434c2bd152fc04713935c4dd
Raouf Abbou <53510613+aabb1998@users.noreply.github.com>
2023-02-16 16:53:10 +1100

    Added firebase.

---
+++ Client/src/Components/MainDonationSection/MainDonationAddToCart.jsx
@@ -0,0 +1,98 @@
+import React, { useState } from "react";
+import arrow from "../../assets/arrow.svg";
+import ellipse from "../../assets/Looper-3.svg";
+
+const MainDonationAddToCart = () => {
+  const [schedule, setSchedule] = useState("onetime");
+  const [selectAmount, setSelectAmount] = useState(100);
+
+  return (
+    <>
+      <div className="MainDonationAddToCart">
+        <div className="MainDonationAddToCart-innerBtns">
+          <div
+            onClick={() => setSchedule("onetime")}
+            className={`innerBtn ${schedule === "onetime" ? "active" : ""}`}
+          >
+            <span>ONE TIMER</span>
+          </div>
+          <div
+            onClick={() => setSchedule("schedule")}
+            className={`innerBtn ${schedule != "onetime" ? "active" : ""}`}
+          >
+            <span>SCHEDULE</span>
+          </div>
+        </div>
+        <div className="MainDonationAddToCart-scheduleInfo">
+          <span>
+            Schedule your gifts to stay consistent with your donations{" "}
+            <img src={arrow} />
+          </span>
+        </div>
+        <div className="MainDonationAddToCart-amounts">
+          <div className="MainDonationAddToCart-amounts-row">
+            <div
+              onClick={() => setSelectAmount(10)}
+              className={`MainDonationAddToCart-amount ${
+                selectAmount === 10 ? "amount-active" : ""
+              }`}
+            >
+              <span>$10</span>
+            </div>
+            <div
+              onClick={() => setSelectAmount(25)}
+              className={`MainDonationAddToCart-amount ${
+                selectAmount === 25 ? "amount-active" : ""
+              }`}
+            >
+              <span>$25</span>
+            </div>
+            <div
+              onClick={() => setSelectAmount(50)}
+              className={`MainDonationAddToCart-amount ${
+                selectAmount === 50 ? "amount-active" : ""
+              }`}
+            >
+              <span>$50</span>
+            </div>
+          </div>
+          <div className="MainDonationAddToCart-amounts-row">
+            <div
+              onClick={() => setSelectAmount(60)}
+              className={`MainDonationAddToCart-amount ${
+                selectAmount === 60 ? "amount-active" : ""
+              }`}
+            >
+              <span>$60</span>
+            </div>
+            <div
+              onClick={() => setSelectAmount(75)}
+              className={`MainDonationAddToCart-amount ${
+                selectAmount === 75 ? "amount-active" : ""
+              }`}
+            >
+              <span>$75</span>
+            </div>
+            <div
+              onClick={() => setSelectAmount(100)}
+              className={`MainDonationAddToCart-amount ${
+                selectAmount === 100 ? "amount-active" : ""
+              }`}
+            >
+              <span>$100</span>
+            </div>
+          </div>
+        </div>
+        <div className="MainDonationAddToCart-input">
+          <span>$</span>
+          <input placeholder="0" />
+        </div>
+        <div className="MainDonationAddToCart-button">
+          <span>Add To Cart</span>
+        </div>
+      </div>
+    </>
+  );
+};
+
+export default MainDonationAddToCart;
---
+++ Client/src/Components/MainProgressBar/MainProgressBar.jsx
@@ -0,0 +1,34 @@
+import React, { useEffect, useState } from "react";
+import "./styles.css";
+
+const MainProgressBar = () => {
+  const [progress, setProgress] = useState(20); // state variable to track progress
+  // useEffect(() => {
+  //   const intervalId = setInterval(() => {
+  //     if (progress < 100) {
+  //       setProgress(progress + 10); // increment progress by 10 every second until it reaches 100
+  //     }
+  //   }, 1000);
+
+  //   return () => {
+  //     clearInterval(intervalId);
+  //   };
+  // }, [progress]);
+  return (
+    <div className="progress-bar">
+      <div className="progress" style={{ width: `${progress}%` }}></div>
+      <div className="progress-info">
+        <div className="progress-info-container">
+          <h4>$1,242</h4>
+          <span>Raised</span>
+        </div>
+        <div className="progress-info-container">
+          <h4>78%</h4>
+          <span>From goal</span>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default MainProgressBar;
