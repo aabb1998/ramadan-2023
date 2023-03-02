d2f4d490535c29c0544adec14fa31507c42686ea
Raouf Abbou <53510613+aabb1998@users.noreply.github.com>
2023-03-03 10:17:27 +1100

    Update UpSellItem.jsx

--- Client/src/Components/UpSells/UpSellItems/UpSellItem.jsx
+++ Client/src/Components/UpSells/UpSellItems/UpSellItem.jsx
@@ -1,22 +1,30 @@
 import React from "react";
 import MiniProgressBar from "../MiniProgressBar/MiniProgressBar";
+import UpsellCart from "../UpsellCart/UpsellCart";
 import "./UpSellItem.css";
 const UpSellItem = ({ item, toggleDiv }) => {
   return (
-    <div className="UpSellItem">
-      <div className="UpSellItem-img">
-        <img src={item?.imgLink} />
+    <>
+      <div className="UpSellItem">
+        <div className="UpSellItem-img">
+          <img src={item?.imgLink} />
+        </div>
+        <div className="UpSellItem-desc">
+          <span>{item?.desc}</span>
+        </div>
+        <div className="UpSellItem-progress">
+          <MiniProgressBar
+            item={item}
+            goal={item?.goal}
+            raised={item?.raised}
+          />
+        </div>
+        <div className="UpSellItem-cart">
+          <button onClick={() => toggleDiv(item)}>Donate</button>
+        </div>
       </div>
-      <div className="UpSellItem-desc">
-        <span>{item?.desc}</span>
-      </div>
-      <div className="UpSellItem-progress">
-        <MiniProgressBar item={item} goal={item?.goal} raised={item?.raised} />
-      </div>
-      <div className="UpSellItem-cart">
-        <button onClick={() => toggleDiv(item)}>Donate</button>
-      </div>
-    </div>
+      {/* <UpsellCart campaign={item} /> */}
+    </>
   );
 };
 
